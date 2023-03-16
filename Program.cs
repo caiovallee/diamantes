c28dd1980c66b9e60f97b5c95444ff73e8668a62
owent <owent@DESKTOP-8NO99AE>
2023-03-16 13:17:41 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dimantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o um numero impar");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("O numero não é impar");
+            }
+            else
+            {
+                int espacos = num / 2;
+                int X = 1;
+
+                for(int i=0; i < num; i++)
+                {
+                    for(int j = 0; j<espacos; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                for(int j = 0 ; j < X; j++) 
+                    {
+                        Console.Write("x");
+                    }
+                    Console.WriteLine();
+
+                    if (i < num / 2)
+                    {
+                        espacos--;
+                        X += 2;
+                    }
+                    else
+                    {
+                        espacos++;
+                        X -= 2;
+                    }
+                    
+                }
+                Console.ReadLine();
+
+            }
+
+        }
+    } 
+
+}
+
